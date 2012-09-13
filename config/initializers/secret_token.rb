edc3c4eb7ebb53f4b8c1ce16c36b7f51d0a1bed6
Marble.Cake <hkr.drm@gmx.com>
2012-09-13 16:03:44 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwogBlog::Application.config.secret_token = 'b5d5acf07ba560ac18c044b37f5f93d0a34a4cff129aa9829f62b09010818caaa3c15fd02e1d462aeb2c74f68822383f579b01a8ff02886f0b426ef7d97e7944'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SwogBlog::Application.config.session_store :cookie_store, :key => '_SwogBlog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SwogBlog::Application.config.session_store :active_record_store
